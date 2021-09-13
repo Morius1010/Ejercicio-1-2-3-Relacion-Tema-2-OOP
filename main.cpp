83be41133e15a04411e946e2a43dc2af76280546
morius <johnmce13@gmail.com>
2021-09-13 14:28:35 +0200

    Exercises 1-2-3 from volume 1

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * @file:   main.cpp
+ * @author: Juan Cabello
+ * @brief: Hacer clases en las que usemos cualquiera de los atributos del ejercicio 1 y un programa de ejemplo
+ * @date 17-02-2021
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class dibujos{
+    private:
+        string lapiz;
+        string libro;
+        string maceta;
+        int polinomio;
+        int elipse;
+    public:
+        dibujos(string, string, string, int, int);
+        //Introducir setter y getters
+};
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
